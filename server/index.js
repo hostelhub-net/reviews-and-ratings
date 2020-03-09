49be01c2a67d710aed25248dd398215c244fc627
Theresa <2litheresa@gmail.com>
2020-03-09 16:42:07 -0700

    Edit paths

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const Review = require('../database/Review.js');
 const router = express.Router();
 
 router.use('/hostels/:hostelId', express.static(`${__dirname}/../client/dist`));
-router.use('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Hostels can be searched by adding /hostels/:hostelId to the end of the URL.');
 });
 
